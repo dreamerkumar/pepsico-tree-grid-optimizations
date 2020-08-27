f3ea26a08c44b2784b68325cf0bb106d2ffa284c
Vishal <vishal@vishals-mbp.attlocal.net>
2020-08-27 18:31:13 -0400

    scaffold for http call

--- src/app/get-keyfigure-data.service.ts
+++ src/app/get-keyfigure-data.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetKeyfigureDataService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getKeyFigureData(): Observable<{}> {
+    const postData = {};
+    return this.httpClient
+      .post(``, postData);
+  }
+
 }
